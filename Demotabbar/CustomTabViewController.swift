f5c0186c0751d8593fbcdd9f5cb75e1e1706959d
Khac vu Nguyen <nguyenkhacvu38@gmail.com>
2019-07-12 17:23:23 +0700

    [update+customtab]

--- Demotabbar/CustomTabViewController.swift
+++ Demotabbar/CustomTabViewController.swift
@@ -11,7 +11,10 @@ public var WEB_VIEW = "web"
 extension Notification.Name {
     static let showColor = Notification.Name("showColor")
 }
+
 class CustomTabViewController: UITabBarController, UITabBarControllerDelegate {
+    var indexSelected = 0
+    @IBOutlet weak var tabarVc: UITabBar!
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -19,18 +22,18 @@ class CustomTabViewController: UITabBarController, UITabBarControllerDelegate {
     }
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
-        self.selectedIndex = 0
+        selectedIndex = self.indexSelected
         tabBar.tintColor = #colorLiteral(red: 0.937254902, green: 0.2078431373, blue: 0.168627451, alpha: 1)
     }
     override func tabBar(_ tabBar: UITabBar, didSelect item: UITabBarItem) {
-   
+        indexSelected = self.selectedIndex
         if item.title == WEB_VIEW {
             tabBar.tintColor = #colorLiteral(red: 0.501960814, green: 0.501960814, blue: 0.501960814, alpha: 1)
             let alertController = UIAlertController(title: "Messenger", message: "Bạn muốn truy cập vào web ", preferredStyle: .actionSheet)
-                alertController.addAction(UIAlertAction(title: "Safari", style: .default, handler: {(action: UIAlertAction) in
+            alertController.addAction(UIAlertAction(title: "Safari", style: .default, handler: {(action: UIAlertAction) in
                 guard let url = URL(string: "https://developer.apple.com/") else { return }
                 UIApplication.shared.open(url)
-                self.selectedIndex = 0
+               self.selectedIndex = self.indexSelected
                 tabBar.tintColor = #colorLiteral(red: 0.937254902, green: 0.2078431373, blue: 0.168627451, alpha: 1)
                 print("Selected item", item.title! )
             }))
@@ -39,13 +42,15 @@ class CustomTabViewController: UITabBarController, UITabBarControllerDelegate {
                 self.present(vc, animated: true, completion: nil)
             }))
             alertController.addAction(UIAlertAction(title: "Cancel", style: .cancel, handler: { (action: UIAlertAction) in
-                self.selectedIndex = 0
+               self.selectedIndex = self.indexSelected
+//                self.selectedIndex = 0
                 tabBar.tintColor = #colorLiteral(red: 0.937254902, green: 0.2078431373, blue: 0.168627451, alpha: 1)
             }))
             alertController.view.tintColor = UIColor.red
             self.present(alertController, animated: true, completion: nil)
             
         }else {
+            
             tabBar.tintColor = #colorLiteral(red: 0.937254902, green: 0.2078431373, blue: 0.168627451, alpha: 1)
         }
     }
